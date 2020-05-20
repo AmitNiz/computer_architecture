cc9724ceaec40f984e0949762a3cd429e117035b
Amit Nizri <amitnzri@gmail.com>
2020-05-20 15:06:16 +0300

    tests script

---
+++ assignment2/tests1/run_tests.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+for i in {1..20}; do
+	cat tests/example$i\_command | xargs ./cacheSim tests/example$i\_trace > our_output$i
+	diff -s ref_results/example$i\_output our_output$i
+	rm our_output$i
+	echo "All done.. "
+done
+
+
+
+
+
