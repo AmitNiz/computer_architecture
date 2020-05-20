309da725dc321b3aa5e8cf89e5fabe48596b902d
Amit Nizri <amitnzri@gmail.com>
2020-05-20 15:09:49 +0300

    tests script

--- assignment2/tests1/run_tests.sh
+++ assignment2/tests1/run_tests.sh
@@ -4,8 +4,8 @@ for i in {1..20}; do
 	cat tests/example$i\_command | xargs ./cacheSim tests/example$i\_trace > our_output$i
 	diff -s ref_results/example$i\_output our_output$i
 	rm our_output$i
-	echo "All done.. "
 done
+echo "All done.. "
 
 
 
