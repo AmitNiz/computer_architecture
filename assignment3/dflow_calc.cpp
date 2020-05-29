d7edd2bc6d0e0b48fb729351c8b34e14e47d093e
OmerHemo <OmerHemo@users.noreply.github.com>
2020-05-29 10:04:29 +0300

    done

--- assignment3/dflow_calc.cpp
+++ assignment3/dflow_calc.cpp
@@ -14,11 +14,11 @@ struct Dependencies{
 };
 
 struct MyProgCtx{
-    int* depth_array;
+    unsigned* depth_array;
     Dependencies* dependencies_array;
-    int numOfInsts;
-    int max_depth;
-    explicit MyProgCtx(unsigned int numOfInsts):depth_array(new int[numOfInsts]),dependencies_array(new Dependencies[numOfInsts])
+    unsigned numOfInsts;
+    unsigned max_depth;
+    explicit MyProgCtx(unsigned int numOfInsts):depth_array(new unsigned[numOfInsts]),dependencies_array(new Dependencies[numOfInsts])
     ,numOfInsts(numOfInsts),max_depth(0){}
     ~MyProgCtx(){
         delete[] this->dependencies_array;
@@ -37,17 +37,17 @@ ProgCtx analyzeProg(const unsigned int opsLatency[], const InstInfo progTrace[],
     }
     MyProgCtx* myProgCtx = new MyProgCtx(numOfInsts);
 
-    for(int i=0;i<numOfInsts; i++){
+    for(unsigned i=0;i<numOfInsts; i++){
         myProgCtx->depth_array[i] = 0;
     }
 
     int last_instuction_write[NUM_OF_REGS];
 
-    for(int i=0; i<NUM_OF_REGS; i++){
+    for(unsigned i=0; i<NUM_OF_REGS; i++){
         last_instuction_write[i] = ENTRY;
     }
 
-    for(int i=0;i < numOfInsts; i++){
+    for(unsigned i=0;i < numOfInsts; i++){
         myProgCtx->dependencies_array[i].src1_incruction_num = last_instuction_write[progTrace[i].src1Idx];
         myProgCtx->dependencies_array[i].src1_opcode = progTrace[last_instuction_write[progTrace[i].src1Idx]].opcode;
         myProgCtx->dependencies_array[i].src2_incruction_num = last_instuction_write[progTrace[i].src2Idx];
@@ -57,9 +57,6 @@ ProgCtx analyzeProg(const unsigned int opsLatency[], const InstInfo progTrace[],
         if(myProgCtx->dependencies_array[i].src1_incruction_num != ENTRY){
             depth_src1 =  myProgCtx->depth_array[myProgCtx->dependencies_array[i].src1_incruction_num];
             latency_src1 = opsLatency[myProgCtx->dependencies_array[i].src1_opcode];
-            if(i == 5){
-                std::cout <<"sr1_dep: " << myProgCtx->dependencies_array[i].src1_incruction_num <<" opcode: "<< myProgCtx->dependencies_array[i].src1_opcode << " latency: " << latency_src1 << std::endl;
-            }
 
         }
         else{
@@ -80,7 +77,7 @@ ProgCtx analyzeProg(const unsigned int opsLatency[], const InstInfo progTrace[],
         last_instuction_write[progTrace[i].dstIdx] = i;
     }
 
-    for(int i=0; i< myProgCtx->numOfInsts ; i++){
+    for(unsigned i=0; i< myProgCtx->numOfInsts ; i++){
         if(myProgCtx->max_depth < myProgCtx->depth_array[i] + opsLatency[progTrace[i].opcode]){
             myProgCtx->max_depth = myProgCtx->depth_array[i] + opsLatency[progTrace[i].opcode];
         }
@@ -95,14 +92,14 @@ void freeProgCtx(ProgCtx ctx) {
 
 int getInstDepth(ProgCtx ctx, unsigned int theInst) {
     MyProgCtx* myProgCtx = (MyProgCtx*)ctx;
-    if(theInst > myProgCtx->numOfInsts) return -1;
+    if(theInst >= myProgCtx->numOfInsts) return -1;
 
     return myProgCtx->depth_array[theInst];
 }
 
 int getInstDeps(ProgCtx ctx, unsigned int theInst, int *src1DepInst, int *src2DepInst) {
     MyProgCtx* myProgCtx = (MyProgCtx*)ctx;
-    if(theInst > myProgCtx->numOfInsts) return -1;
+    if(theInst >= myProgCtx->numOfInsts) return -1;
     *src1DepInst = myProgCtx->dependencies_array[theInst].src1_incruction_num;
     *src2DepInst = myProgCtx->dependencies_array[theInst].src2_incruction_num;
     return 0;
